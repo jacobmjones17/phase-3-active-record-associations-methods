5115a9e1863a0509546f4ba78c0e5d666034e6e4
Jacob <jacobmjones17@gmail.com>
2022-09-13 13:08:45 -0400

    commit tree 5ac9cb680146f54493ec195c58799dcb399eb9b6

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,16 +1,23 @@
+require "pry"
 class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    # binding.pry
+    all_artists = self.artists.all
+
+    all_artists.map { |obj| obj.name }
   end
 end
